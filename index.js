f5ff45d3cd26f3a322abf7a0f15584b245e212a3
JCJ <jesse.jackson.c@gmail.com>
2020-09-16 20:37:49 -0500

    Psuedo template structure

---
+++ index.js
@@ -0,0 +1,30 @@
+// Start Program
+
+    // Ask user for Title
+        // User Input
+
+    // Ask user for Description
+            // User Input
+
+    // Ask user for Table of Contents
+            // User Input
+
+    // Ask user for Installation
+            // User Input
+
+    // Ask user for Usage
+            // User Input
+
+    // Ask user for License
+            // User Input
+
+    // Ask user for Contributing
+            // User Input
+
+    // Ask user for Tests
+            // User Input
+
+    // Ask user for Questions
+            // User Input
+
+// End Program
