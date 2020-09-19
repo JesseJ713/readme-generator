3a1057b21fd48aeb3cc56ca6399cb5ef648ac24e
JCJ <jesse.jackson.c@gmail.com>
2020-09-19 11:43:19 -0500

    Established object that will provide prompts to users

--- index.js
+++ index.js
@@ -4,6 +4,57 @@ const fs = require("fs");
 const util = require("util");
 const generateMarkdown = require("./generateMarkdown");
 
+function userPrompts() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Please enter the title of the application" 
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please provide a unique, thorough description of the application" 
+    },
+    {
+      type: "input",
+      name: "tableOfContents",
+      message: "Please provide table of contents" 
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Please provide installation" 
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please provide usage" 
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "Please provide license" 
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Please provide contributing" 
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Please provide tests" 
+    },
+    {
+      type: "input",
+      name: "questions",
+      message: "Please provide questions" 
+    },
+  ]);
+}
+
+userPrompts();
     // Ask user for Title
         // User Input
 
