0ae24b0ea8833b7c58aead4864479ba385ffbcce
JCJ <jesse.jackson.c@gmail.com>
2020-09-19 11:56:28 -0500

    Updating license prompt to list and added all options for licensing within Github

--- index.js
+++ index.js
@@ -32,8 +32,9 @@ function userPrompts() {
       message: "Please provide usage" 
     },
     {
-      type: "input",
+      type: "list",
       name: "license",
+      choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause Simplified License", "BSD 3-Clause New/Revised License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser Public License v2.1", "Mozilla Public License 2.0", "The Unilicense"],
       message: "Please provide license" 
     },
     {
